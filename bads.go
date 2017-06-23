38e140f82a621f66181c68c482054e9251d697ce
Mykola Perehinets <mperehin@softservecom.com>
2017-06-23 19:45:23 +0300

    Initial commit

---
+++ bads.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
+
+	"github.com/gorilla/context"
+	"github.com/gorilla/sessions"
+)
+
+type user struct {
+	Login    string
+	Password string
+}
+
+var (
+	store  = sessions.NewCookieStore([]byte("33446a9dcf9ea060a0a6532b166da32f304af0de"))
+	userdb user
+	tpl    *template.Template
+)
+
+func index(w http.ResponseWriter, req *http.Request) {
+	logFunc("Client " + req.RemoteAddr + " visited to /")
+
+	if alreadyLoggedIn(w, req) {
+		http.Redirect(w, req, "/todo", http.StatusSeeOther)
+	}
+
+	tpl.ExecuteTemplate(w, "index.html", false)
+}
+
+func todo(w http.ResponseWriter, req *http.Request) {
+	logFunc("Client " + req.RemoteAddr + " visited to /todo")
+
+	if !alreadyLoggedIn(w, req) {
+		http.Redirect(w, req, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		hostname := req.FormValue("hostname")
+		ipaddress := req.FormValue("ipaddress")
+
+		fmt.Println(hostname, ipaddress)
+		logFunc("Client " + req.RemoteAddr + " deployed Bacula Agent to host (FQDN): " + req.FormValue("hostname") + ", host IP ADDRESS: " + req.FormValue("ipaddress"))
+
+		arg := "-la"
+		out, _ := exec.Command("ls", arg).Output()
+		io.WriteString(w, fmt.Sprintf("%s", out))
+		// tpl.ExecuteTemplate(w, "done.html", nil)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "todo.html", nil)
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	logFunc("Client " + req.RemoteAddr + " visited to /login")
+
+	session, err := store.Get(req, "session")
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+
+		if un != userdb.Login && p != userdb.Password {
+			http.Error(w, "", http.StatusForbidden)
+			http.Error(w, "ERROR: Username and/or password do not match. So, verify it and please relogin. Thank you for understanding.", http.StatusForbidden)
+			logFunc("Client " + req.RemoteAddr + " visited to /login but ERROR: Username and/or password do not match." + " Username: " + req.FormValue("username") + ", Password: " + req.FormValue("password"))
+			http.Redirect(w, req, "/login", http.StatusSeeOther)
+			// tpl.ExecuteTemplate(w, "done.html", nil)
+			return
+		}
+
+		session.Values["username"] = un
+		session.Save(req, w)
+
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "login.html", nil)
+}
+
+func logout(w http.ResponseWriter, req *http.Request) {
+	logFunc("Client " + req.RemoteAddr + " visited to /logout")
+	session, _ := store.Get(req, "session")
+
+	session.Values["username"] = ""
+	session.Save(req, w)
+
+	// http.Redirect(w, req, "/login", http.StatusSeeOther)
+	tpl.ExecuteTemplate(w, "logout.html", nil)
+}
+
+func alreadyLoggedIn(w http.ResponseWriter, req *http.Request) bool {
+	session, err := store.Get(req, "session")
+	if err != nil {
+		return false
+	}
+
+	username, found := session.Values["username"]
+	if found && username == userdb.Login {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+
+	userdb.Login = "admin"
+	userdb.Password = "admin"
+
+	store.Options = &sessions.Options{
+		// Domain:   "localhost.localdomain",
+		Path:     "/",
+		MaxAge:   10 * 60,
+		Secure:   true,
+		HttpOnly: true,
+	}
+
+	tpl = template.Must(template.ParseGlob("templates/*"))
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/todo", todo)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/done", done)
+	http.HandleFunc("favicon.ico", faviconHandler)
+	log.Printf("Starting Bacula Agent Deploy Server front-end(web) service...")	
+	log.Printf("BADS about to listen on 8443. Go to https://127.0.0.1:8443 for verifing...")
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
+	err := http.ListenAndServeTLS(":8443", "server.crt", "server.pem", context.ClearHandler(http.DefaultServeMux))
+	log.Fatal(err)
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "/images/favicon.ico")
+}
+
+func done(w http.ResponseWriter, req *http.Request) {
+	logFunc("Client " + req.RemoteAddr + " visited to /done")
+
+	tpl.ExecuteTemplate(w, "done.html", false)
+}
+
+func logFunc(l string) {
+	log.Println(l)
+}
---
+++ bads.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this file. If not, see <http://www.gnu.org/licenses/>.
+##
+VERSION=23062017
+##
+set -e
+case "$OSTYPE" in
+  linux*)   OS="linux"  ;;
+  *)        echo "Your operating system ('$OSTYPE') is not supported by BADS. Exiting." && exit 1 ;;
+esac
+##
+LOGDIR=/var/log/bads
+DIR=/opt/bads
+DATE=$(date +%Y-%m-%d_%H:%M)
+##
+if [[ ! -e $LOGDIR ]]; then
+    mkdir -p $LOGDIR
+elif [[ ! -d $LOGDIR ]]; then
+    echo "ERROR: $LOGDIR already exists but is not a directory... Please fix..." 2>&1
+fi
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR
+#sudo su root
+#cd /etc/ansible/roles/InstallBaculaAgent
+#ssh-agent bash
+#ssh-add /root/.ssh/id_rsa
+#sleep 3
+cd $DIR
+echo "-----------------------------------------------------------------------------------------------------------------" >> $LOGDIR/bads.log 2>&1
+echo "Starting The Bacula Agent Deploy Server (ver.$VERSION)... Starting at $DATE..." >> $LOGDIR/bads.log 2>&1
+$DIR/bads >> $LOGDIR/bads.log 2>&1 &
+netstat -ntulp | grep bads >> $LOGDIR/bads.log 2>&1
+echo "Bacula Agent Deploy Server (ver.$VERSION) has been started successfully..."
+exit 0
